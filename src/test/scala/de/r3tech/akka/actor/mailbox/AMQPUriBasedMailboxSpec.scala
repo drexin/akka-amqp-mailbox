e14ef09757e9ee58d76815e5df9e7b9ca01c12cd
Marius Stroe <laurentiu.stroe@gmail.com>
2013-05-06 19:11:55 +0300

    AMQP: Added test for URI based connection.

---
+++ src/test/scala/de/r3tech/akka/actor/mailbox/AMQPUriBasedMailboxSpec.scala
@@ -0,0 +1,18 @@
+package de.r3tech.akka.actor.mailbox
+
+import akka.actor.mailbox.DurableMailboxSpec
+
+object AMQPUriBasedMailboxSpec {
+  val config = """
+    AMQP-dispatcher {
+      mailbox-type = de.r3tech.akka.actor.mailbox.AMQPBasedMailboxType
+      throughput = 1
+      uri = "amqp://127.0.0.1"
+    }
+    """
+}
+
+@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+class AMQPUriBasedMailboxSpec extends DurableMailboxSpec("AMQP", AMQPUriBasedMailboxSpec.config) {
+  val settings = new AMQPBasedMailboxSettings(system, system.settings.config.getConfig("AMQP-dispatcher"))
+}
